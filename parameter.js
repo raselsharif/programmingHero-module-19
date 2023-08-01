64013e6bde4865cd6cf302c38ffed865b6e9e2c6
raselsharif <raselsharif9@gmail.com>
2023-08-01 20:39:31 +0600

    more about function

--- parameter.js
+++ parameter.js
@@ -1,9 +1,24 @@
-function giveMeSingara (money){
+// function giveMeSingara (money){
+//     console.log(money, "taka nen");
+//     console.log("Amake", money,"takar singara den..");
+// }
+
+// giveMeSingara(100);
+// giveMeSingara(200);
+// giveMeSingara(150);
+// giveMeSingara(120);
+
+// function sum (a,b){
+//     console.log(a,b);
+//     var sum = a+b;
+//     console.log(sum);
+// }
+
+// sum(2,4)
+
+function add(money) {
     console.log(money, "taka nen");
-    console.log("Amake", money,"takar singara den..");
 }
 
-giveMeSingara(100);
-giveMeSingara(200);
-giveMeSingara(150);
-giveMeSingara(120);
+var taka= 10;
+add(taka)
