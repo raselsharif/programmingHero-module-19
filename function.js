3511ba25ee1a85cdba58eaf9f9c2da83335bfbb6
raselsharif <raselsharif9@gmail.com>
2023-08-01 20:18:54 +0600

    added function and parameter

---
+++ function.js
@@ -0,0 +1,16 @@
+// === function declare ====
+
+function startFan (){
+    console.log("Stand up");
+    console.log("go to near switch");
+    console.log("then press switch");
+}
+
+
+// call the function 
+
+startFan();
+
+console.log("=======lunch time=========");
+
+startFan();
---
+++ parameter.js
@@ -0,0 +1,9 @@
+function giveMeSingara (money){
+    console.log(money, "taka nen");
+    console.log("Amake", money,"takar singara den..");
+}
+
+giveMeSingara(100);
+giveMeSingara(200);
+giveMeSingara(150);
+giveMeSingara(120);
