2f2f403de101bbba0f1bee2708290da3e37a7a21
raselsharif <raselsharif9@gmail.com>
2023-08-01 22:03:16 +0600

    added function add

---
+++ function-add.js
@@ -0,0 +1,14 @@
+function result(num1, num2) {
+    var total = num1 + num2;
+    return total;
+}
+
+const result1 = result(50, 60);
+const result2 = result(55, 50);
+const result3 = result(40, 30);
+const result4 = result(40, 30);
+
+
+const finalResult = result1 + result2 + result3 + result4;
+
+console.log(finalResult);
