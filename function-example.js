960d97dd5804f70aa6c883ae090f4bb60a23e6d3
raselsharif <raselsharif9@gmail.com>
2023-08-01 21:50:32 +0600

    added return example

---
+++ function-example.js
@@ -0,0 +1,13 @@
+function assignment(assignment1, assignment2, assignment3) {
+    const totalMarks = assignment1 + assignment2 + assignment3;
+    const average = totalMarks / 3;
+    return average;
+}
+
+const assignment1Mark = 60;
+const assignment2Mark = 50;
+const assignment3Mark = 40;
+
+let assignmentMarks = assignment(assignment1Mark,assignment2Mark,assignment3Mark);
+parseInt().assignmentMarks;
+console.log("my avarage is: ",assignmentMarks);
